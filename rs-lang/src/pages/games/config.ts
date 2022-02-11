b2dc2a615243c61f93efd6e322882ecfcfe31b6b
MikolaSh <nikolashulga2000@gmail.com>
2022-02-12 02:48:34 +0300

    feat: update config

--- rs-lang/src/pages/games/config.ts
+++ rs-lang/src/pages/games/config.ts
@@ -1,2 +1,4 @@
 export const gamesPageId = 'games';
 export const gamePageClassName = 'game-page';
+export const startGameButton = 'start-game';
+export const unitСheckbox = 'unit-item-checkbox';
--- rs-lang/src/pages/games/sprint/config.ts
+++ rs-lang/src/pages/games/sprint/config.ts
@@ -1,1 +1,7 @@
 export const sprintPageId = 'sprint';
+export const timerClassName = 'timer';
+export const scoreClassName = 'score';
+export const wordTranslation = 'word-translate-container';
+export const answersContainer = 'answer-buttons';
+export const timerDuration = 30000;
+export const timerInterval = 1000;

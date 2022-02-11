5361e093ca516e82514858cf5e4eb29c46d96d55
MikolaSh <nikolashulga2000@gmail.com>
2022-02-12 02:48:57 +0300

    feat: start 'startGame' function

--- rs-lang/src/pages/games/sprint/index.ts
+++ rs-lang/src/pages/games/sprint/index.ts
@@ -1,6 +1,20 @@
 import { renderMarkup } from '../../../utilities/renderMarkup';
-import { sprintPageMarkup } from './markup';
+import { startGameButton } from '../config';
+import { sprintGameMarkup, sprintPageMarkup } from './markup';
+import { sprintPageId } from './config';
+import { setCountdown } from './timer';
+
+
+
+const startGame = async () => {
+  const sprintContainer = document.body.querySelector(`#${sprintPageId}`) as HTMLElement;
+  sprintContainer.innerHTML = '';
+  renderMarkup(sprintContainer, sprintGameMarkup);
+  setCountdown();
+}
 
 export const mountSprintPageDOMElement = (parentDOMElement: HTMLElement) => {
   renderMarkup(parentDOMElement, sprintPageMarkup);
+  const buttonStartGame = document.querySelector(`.${startGameButton}`)
+  buttonStartGame?.addEventListener('click', startGame, {once: true});
 };

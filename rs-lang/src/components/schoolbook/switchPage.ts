37bba9540d6c3fb8a58699083f3972b4c485bc9e
alchonokk <galunia_suglob@mail.ru>
2022-02-12 10:21:13 +0300

    refactor: delete spaces

--- rs-lang/src/components/schoolbook/switchPage.ts
+++ rs-lang/src/components/schoolbook/switchPage.ts
@@ -4,42 +4,42 @@ import { createCardsOfBook } from "./createCardsOfWord";
 
 
 export const updateStatePageOfBook = async () => {
-    const cardsPage = appState.numberPageOfSchoolbook + 1;
+  const cardsPage = appState.numberPageOfSchoolbook + 1;
+
+  if (cardsPage >= 1) {
+    (document.querySelector(nextPageOfBookClassName) as HTMLButtonElement).disabled = false;
+  } else {
+    (document.querySelector(nextPageOfBookClassName) as HTMLButtonElement).disabled = true;
+  }
+
+  if (cardsPage === 1) {
+    (document.querySelector(prevPageOfBookClassName) as HTMLButtonElement).disabled = true;
+  } else {
+    (document.querySelector(prevPageOfBookClassName) as HTMLButtonElement).disabled = false;
+  }
+
+  if (cardsPage === 30) {
+    (document.querySelector(nextPageOfBookClassName) as HTMLButtonElement).disabled = true;
+  } else {
+    (document.querySelector(nextPageOfBookClassName) as HTMLButtonElement).disabled = false;
+  }
+};
   
-    if (cardsPage >= 1) {
-      (document.querySelector(nextPageOfBookClassName) as HTMLButtonElement).disabled = false;
-    } else {
-      (document.querySelector(nextPageOfBookClassName) as HTMLButtonElement).disabled = true;
-    }
+export const rerenderPageOfBook = () => {
+  createCardsOfBook();
+  rerenderPagination();
+  updateStatePageOfBook();
+};
   
-    if (cardsPage === 1) {
-      (document.querySelector(prevPageOfBookClassName) as HTMLButtonElement).disabled = true;
-    } else {
-      (document.querySelector(prevPageOfBookClassName) as HTMLButtonElement).disabled = false;
-    }
+export const switchPageOfBook = () => {
+  document.querySelector(nextPageOfBookClassName)?.addEventListener('click', () => {
+    appState.numberPageOfSchoolbook += 1;
+    rerenderPageOfBook();
+  });
   
-    if (cardsPage === 30) {
-      (document.querySelector(nextPageOfBookClassName) as HTMLButtonElement).disabled = true;
-    } else {
-      (document.querySelector(nextPageOfBookClassName) as HTMLButtonElement).disabled = false;
-    }
-  };
-  
-  export const rerenderPageOfBook = () => {
-    createCardsOfBook();
-    rerenderPagination();
-    updateStatePageOfBook();
-  };
-  
-  export const switchPageOfBook = () => {
-    document.querySelector(nextPageOfBookClassName)?.addEventListener('click', () => {
-      appState.numberPageOfSchoolbook += 1;
-      rerenderPageOfBook();
-    });
-  
-    document.querySelector(prevPageOfBookClassName)?.addEventListener('click', () => {
-      appState.numberPageOfSchoolbook -= 1;
-      rerenderPageOfBook();
-    });
-  };
+  document.querySelector(prevPageOfBookClassName)?.addEventListener('click', () => {
+    appState.numberPageOfSchoolbook -= 1;
+    rerenderPageOfBook();
+  });
+};
   

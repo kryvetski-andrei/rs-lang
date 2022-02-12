09ee0add28934c7e75f4afafa11f8058985b4f5b
MikolaSh <nikolashulga2000@gmail.com>
2022-02-12 22:27:50 +0300

    feat: add class-names fot checkboxes

--- rs-lang/src/pages/games/sprint/markup.ts
+++ rs-lang/src/pages/games/sprint/markup.ts
@@ -1,6 +1,7 @@
 import { pageClassName } from '../../config';
 import { answersContainer, scoreClassName, sprintPageId, timerClassName, wordTranslation } from './config';
 import { gamePageClassName } from '../config';
+import { unitСheckbox } from '../config';
 
 import '../index.scss';
 import './index.scss';
@@ -12,12 +13,12 @@ export const sprintPageMarkup = `
     <p class="game-description">Выбирать варианты ответа можно как и нажатием на кнопки "Верно"/"Неверно", так и нажатием клавишь стрелок</p>
     <p class="game-description">Выбирете сложность</p>
     <div class="difficulty-selection-container">
-      <input type="radio" id="1" name="unit"><label class="unit-item" for="1">A1</label>
-      <input type="radio" id="2" name="unit"><label class="unit-item" for="2">A2</label>
-      <input type="radio" id="3" name="unit"><label class="unit-item" for="3">B1</label>
-      <input type="radio" id="4" name="unit"><label class="unit-item" for="4">B2</label>
-      <input type="radio" id="5" name="unit"><label class="unit-item" for="5">C1</label>
-      <input type="radio" id="6" name="unit"><label class="unit-item" for="6">C2</label>
+      <input class="${unitСheckbox}" type="radio" id="1" name="unit"><label class="unit-item" for="1">A1</label>
+      <input class="${unitСheckbox}" type="radio" id="2" name="unit"><label class="unit-item" for="2">A2</label>
+      <input class="${unitСheckbox}" type="radio" id="3" name="unit"><label class="unit-item" for="3">B1</label>
+      <input class="${unitСheckbox}" type="radio" id="4" name="unit"><label class="unit-item" for="4">B2</label>
+      <input class="${unitСheckbox}" type="radio" id="5" name="unit"><label class="unit-item" for="5">C1</label>
+      <input class="${unitСheckbox}" type="radio" id="6" name="unit"><label class="unit-item" for="6">C2</label>
     </div>
     <button class="start-game">Начать</button>
   </section>

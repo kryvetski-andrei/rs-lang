796b60c0cb4909a3f5158d994a287e4e66b4a47a
alchonokk <galunia_suglob@mail.ru>
2022-02-12 10:00:13 +0300

    feat: add config

--- rs-lang/src/pages/schoolbook/config.ts
+++ rs-lang/src/pages/schoolbook/config.ts
@@ -1,1 +1,17 @@
 export const schoolbookPageId = 'schoolbook';
+
+export const boxCardsIdName = 'cards-block';
+export const unitsClassName = 'unit-button';
+export const prevPageOfBookClassName = '.prev-button';
+export const nextPageOfBookClassName = '.next-button';
+export const paginationClassName = '.number-page';
+
+export type Data = {
+  numberPageOfSchoolbook: number;
+  groupOfSchoolbook: number;
+};
+
+export const appState: Data = {
+  numberPageOfSchoolbook: 0,
+  groupOfSchoolbook: 0,
+};

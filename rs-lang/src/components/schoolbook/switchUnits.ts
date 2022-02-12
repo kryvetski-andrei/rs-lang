272e4d046132f6fba7781b92872a13bd92d82e63
alchonokk <galunia_suglob@mail.ru>
2022-02-12 10:21:24 +0300

    refactor: delete spaces

--- rs-lang/src/components/schoolbook/switchUnits.ts
+++ rs-lang/src/components/schoolbook/switchUnits.ts
@@ -2,14 +2,14 @@ import { appState, unitsClassName } from "../../pages/schoolbook/config";
 import { rerenderPageOfBook } from "./switchPage";
 
 export const switchUnitsOfBook = () => {
-    document.body.addEventListener('click', (e: Event) => {
-      const target = e.target as HTMLElement;
-      if (target.classList.contains(unitsClassName)) {
-        const titleOfUnit = target.getAttribute('data');
-        appState.groupOfSchoolbook = Number(titleOfUnit);
-        appState.numberPageOfSchoolbook = 0;
-        rerenderPageOfBook();
-      }
-    });
-  };
+  document.body.addEventListener('click', (e: Event) => {
+    const target = e.target as HTMLElement;
+    if (target.classList.contains(unitsClassName)) {
+      const titleOfUnit = target.getAttribute('data');
+      appState.groupOfSchoolbook = Number(titleOfUnit);
+      appState.numberPageOfSchoolbook = 0;
+      rerenderPageOfBook();
+    }
+  });
+};
   

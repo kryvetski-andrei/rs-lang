2752dcaf6ae16f12caddb69dc709158283564461
alchonokk <galunia_suglob@mail.ru>
2022-02-12 10:20:33 +0300

    refactor: delete spaces

--- rs-lang/src/components/schoolbook/switchAudio.ts
+++ rs-lang/src/components/schoolbook/switchAudio.ts
@@ -1,24 +1,24 @@
 import toggleAudio from "./toggleAudio";
 
 export const switchAudio = (word: string) => {
-    document.querySelector(`.audio-button-${word}`)?.addEventListener('click', (e: Event) => {
-      const target = e.target as HTMLElement;
-      const titleOfAudio = target.getAttribute('data-audio');
-      const titleOfAudioMeaning = `${titleOfAudio?.slice(0, -4)}_meaning.mp3`;
-      const titleOfAudioExample = `${titleOfAudio?.slice(0, -4)}_example.mp3`;
-      if (titleOfAudio) {
-        toggleAudio(titleOfAudio, titleOfAudioMeaning, titleOfAudioExample);
-      }
-    });
-  
-    document.querySelector(`.learn-button-${word}`)?.addEventListener('click', (e: Event) => {
-      const target = e.target as HTMLElement;
-      target.classList.toggle('studied');
-    });
-  
-    document.querySelector(`.difficult-button-${word}`)?.addEventListener('click', (e: Event) => {
-      const target = e.target as HTMLElement;
-      target.classList.toggle('difficult');
-    });
-  };
+  document.querySelector(`.audio-button-${word}`)?.addEventListener('click', (e: Event) => {
+    const target = e.target as HTMLElement;
+    const titleOfAudio = target.getAttribute('data-audio');
+    const titleOfAudioMeaning = `${titleOfAudio?.slice(0, -4)}_meaning.mp3`;
+    const titleOfAudioExample = `${titleOfAudio?.slice(0, -4)}_example.mp3`;
+    if (titleOfAudio) {
+      toggleAudio(titleOfAudio, titleOfAudioMeaning, titleOfAudioExample);
+    }
+  });
+
+  document.querySelector(`.learn-button-${word}`)?.addEventListener('click', (e: Event) => {
+    const target = e.target as HTMLElement;
+    target.classList.toggle('studied');
+  });
+
+  document.querySelector(`.difficult-button-${word}`)?.addEventListener('click', (e: Event) => {
+    const target = e.target as HTMLElement;
+    target.classList.toggle('difficult');
+  });
+};
   

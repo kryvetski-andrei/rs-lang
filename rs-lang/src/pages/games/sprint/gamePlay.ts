13eb1a50950574e3053559dc97e9baec6cd4a1f7
MikolaSh <nikolashulga2000@gmail.com>
2022-02-13 01:53:55 +0300

    feat: change word pairs function

---
+++ rs-lang/src/pages/games/sprint/gamePlay.ts
@@ -0,0 +1,19 @@
+import { IPair } from "../../../interfaces";
+import { wordTranslation } from "./config";
+
+let currentPair = 0;
+
+
+const showCurrentPair = (wordPairs: Array<IPair>) => {
+  (document.body.querySelector(`.${wordTranslation}`) as HTMLElement).innerHTML = `${wordPairs[currentPair].wordsPair}`;
+}
+
+export const setAnswer = (wordPairs: Array<IPair>, userAnswer: string) => {
+  if (String(wordPairs[currentPair].isPairRight) === userAnswer){
+    wordPairs[currentPair].userCorrect = true;
+  } else {
+    wordPairs[currentPair].userCorrect = false;
+  }
+  currentPair++;
+  showCurrentPair(wordPairs);
+}

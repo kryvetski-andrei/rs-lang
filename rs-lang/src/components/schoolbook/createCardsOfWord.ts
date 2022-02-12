c319b6b588e1158492419c4a5ee3394ff5cdceaa
alchonokk <galunia_suglob@mail.ru>
2022-02-12 14:13:44 +0300

    feat: add function localstor in createCardsOfBook

--- rs-lang/src/components/schoolbook/createCardsOfWord.ts
+++ rs-lang/src/components/schoolbook/createCardsOfWord.ts
@@ -3,15 +3,20 @@ import { IWord } from "../../interfaces";
 import { appState, boxCardsIdName } from "../../pages/schoolbook/config";
 import { getWords } from "../../utilities/api";
 import { cleanUp } from "../../utilities/cleanUp";
+import { groupOfSchoolbookLocalStorage, pageOfBookLocalStorage } from "../config";
+import { addPageOfBookInLocalStorage, addUnitOfBookInLocalStorage } from "./localStorageOfBook";
 import { switchAudio } from "./switchAudio";
 
 export const createCardsOfBook = async () => {
   const cardOfBookElement = document.getElementById(boxCardsIdName) as HTMLElement;
    
+  addPageOfBookInLocalStorage(); 
+  addUnitOfBookInLocalStorage();
+
   const wordsData  = await getWords(appState.numberPageOfSchoolbook, appState.groupOfSchoolbook);
-    
-    // console.log(wordsData);
+  
   cleanUp(cardOfBookElement);
+  
   const numericOfUnit = String(appState.groupOfSchoolbook + 1);
   wordsData.forEach((el: IWord) => {
     mountCardOfBookDOMElement(cardOfBookElement, el, numericOfUnit);

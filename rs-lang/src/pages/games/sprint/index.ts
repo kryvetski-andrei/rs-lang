12cf7a6c2afc2beeddb9f0aa4b33f2dda63d6d0a
MikolaSh <nikolashulga2000@gmail.com>
2022-02-13 01:54:47 +0300

    feat: add gamePlay and gameover

--- rs-lang/src/pages/games/sprint/index.ts
+++ rs-lang/src/pages/games/sprint/index.ts
@@ -1,9 +1,26 @@
 import { renderMarkup } from '../../../utilities/renderMarkup';
-import { startGameButton } from '../config';
-import { sprintGameMarkup, sprintPageMarkup } from './markup';
-import { sprintPageId } from './config';
+import { startGameButton, unitСheckbox } from '../config';
+import { resultsMarkup, sprintGameMarkup, sprintPageMarkup } from './markup';
+import { answersContainer, resultNavigationButtons, playAgainButtonClassName, rightAnswersContainer, sprintPageId, toSchoolbookButtonClassName, wordTranslation, wrongAnswersContainer, timerDuration } from './config';
 import { setCountdown } from './timer';
+import { getWords } from '../../../utilities/api';
+import { IPair, IWord } from '../../../interfaces'
+import { generatePairs } from './getWordsPairs';
+import { showResults } from './endGame';
+import { setAnswer } from './gamePlay';
 
+
+
+
+const getCurrentGroupOfWords = (): number => {
+  let groupIndex = 0;
+  document.body.querySelectorAll(`.${unitСheckbox}`)?.forEach((elem, index) => {
+    if((elem as HTMLInputElement).checked){
+      groupIndex = index;
+    }
+  })
+  return groupIndex;
+}
 
 
 const startGame = async () => {
@@ -11,6 +28,15 @@ const startGame = async () => {
   sprintContainer.innerHTML = '';
   renderMarkup(sprintContainer, sprintGameMarkup);
   setCountdown();
+  const wordPairs = generatePairs(await getWords(0, getCurrentGroupOfWords()));
+  setTimeout(() => {
+    showResults(wordPairs, sprintContainer);
+  }, timerDuration)
+  document.body.querySelector(`.${answersContainer}`)?.addEventListener('click',({target}) =>{
+    if((target as Element).tagName === 'BUTTON'){
+      setAnswer(wordPairs, (target as Element).className);
+    }
+  })
 }
 
 export const mountSprintPageDOMElement = (parentDOMElement: HTMLElement) => {

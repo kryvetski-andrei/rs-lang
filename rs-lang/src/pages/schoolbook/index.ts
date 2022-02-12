2aaea9da2040680286098b635331699e219a918b
alchonokk <galunia_suglob@mail.ru>
2022-02-12 10:03:12 +0300

    feat: add main block function for book

--- rs-lang/src/pages/schoolbook/index.ts
+++ rs-lang/src/pages/schoolbook/index.ts
@@ -1,6 +1,20 @@
+import { mountWrapBookDOMElement } from '../../components/schoolbook';
+import { createCardsOfBook } from '../../components/schoolbook/createCardsOfWord';
+import { switchPageOfBook, updateStatePageOfBook } from '../../components/schoolbook/switchPage';
+import { switchUnitsOfBook } from '../../components/schoolbook/switchUnits';
 import { renderMarkup } from '../../utilities/renderMarkup';
+import { schoolbookPageId } from './config';
 import { schoolbookPageMarkup } from './markup';
 
 export const mountSchoolbookPageDOMElement = (parentDOMElement: HTMLElement) => {
   renderMarkup(parentDOMElement, schoolbookPageMarkup);
+
+  const schoolbookPageElement = document.body.querySelector(`#${schoolbookPageId}`) as HTMLElement;
+  mountWrapBookDOMElement(schoolbookPageElement);
+
+  
+  createCardsOfBook();
+  updateStatePageOfBook();
+  switchUnitsOfBook();
+  switchPageOfBook();
 };

5dca5c1be3e10909d1c9e7e70f5921d0bdee8b8e
alchonokk <galunia_suglob@mail.ru>
2022-02-13 14:08:13 +0300

    feat: add function for show area learn hard button

--- rs-lang/src/components/schoolbook/createCardsOfWord.ts
+++ rs-lang/src/components/schoolbook/createCardsOfWord.ts
@@ -1,9 +1,11 @@
 import { mountCardOfBookDOMElement } from '.';
 import { IWord } from '../../interfaces';
 import { appState, boxCardsIdName } from '../../pages/schoolbook/config';
-import { getWords } from '../../utilities/api';
+import { getUserWords, getWords } from '../../utilities/api';
+import { userDataLocalStorage } from '../../utilities/api/config';
 import { cleanUp } from '../../utilities/cleanUp';
 import { addPageOfBookInLocalStorage, addUnitOfBookInLocalStorage } from './localStorageOfBook';
+import { showArea } from './showAreaDifficultAndLearn';
 import { switchAudio } from './switchAudio';
 
 export const createCardsOfBook = async () => {
@@ -12,13 +14,29 @@ export const createCardsOfBook = async () => {
   addPageOfBookInLocalStorage();
   addUnitOfBookInLocalStorage();
 
-  const wordsData = await getWords(appState.numberPageOfSchoolbook, appState.groupOfSchoolbook);
+  const wordsForPage = await getWords(appState.numberPageOfSchoolbook, appState.groupOfSchoolbook);
 
   cleanUp(cardOfBookElement);
 
   const numericOfUnit = String(appState.groupOfSchoolbook + 1);
-  wordsData.forEach((el: IWord) => {
-    mountCardOfBookDOMElement(cardOfBookElement, el, numericOfUnit);
-    switchAudio(el.word);
-  });
+
+  if ((localStorage.getItem(`${userDataLocalStorage}`)) !== null) {
+    const userDataForBook = JSON.parse(localStorage.getItem(`${userDataLocalStorage}`)!);
+    let arrayOfUserWord= await getWordsOfUser(userDataForBook.userId);
+    wordsForPage.forEach((wordOfPage: IWord) => {
+      mountCardOfBookDOMElement(cardOfBookElement, wordOfPage, numericOfUnit);
+      switchAudio(wordOfPage.word);
+      showArea(wordOfPage.word, wordOfPage.id, userDataForBook.userId, arrayOfUserWord);
+    })
+  } else {
+    wordsForPage.forEach((wordOfPage: IWord) => {
+      mountCardOfBookDOMElement(cardOfBookElement, wordOfPage, numericOfUnit);
+      switchAudio(wordOfPage.word);
+    })   
+  }
 };
+
+export const getWordsOfUser = async(userId:string)=> {
+  const arrayOfWords = await getUserWords (userId);
+  return arrayOfWords;
+}

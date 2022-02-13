7de88a3aab5c0080139a9c4731149b0e105672b2
alchonokk <galunia_suglob@mail.ru>
2022-02-13 17:52:25 +0300

    refactor: change size of tab

--- rs-lang/src/components/schoolbook/markup.ts
+++ rs-lang/src/components/schoolbook/markup.ts
@@ -4,51 +4,49 @@ import { apiBaseURL } from '../../utilities/api/config';
 import { MAX_NUMBER_OF_WORDS_PER_PAGE } from '../config';
 
 export const wrapOfBook = `
-<div class="forms">
+  <div class="forms">
     <div class="unit-controls">
-        <p class="units-text">Units</p>
-        <button class="unit-1 unit-button" title="Unit 1" data="0">1</button>
-        <button class="unit-2 unit-button" title="Unit 2" data="1">2</button>
-        <button class="unit-3 unit-button" title="Unit 3" data="2">3</button>
-        <button class="unit-4 unit-button" title="Unit 4" data="3">4</button>
-        <button class="unit-5 unit-button" title="Unit 5" data="4">5</button>
-        <button class="unit-6 unit-button" title="Unit 6" data="5">6</button>
+      <p class="units-text">Units</p>
+      <button class="unit-1 unit-button" title="Unit 1" data="0">1</button>
+      <button class="unit-2 unit-button" title="Unit 2" data="1">2</button>
+      <button class="unit-3 unit-button" title="Unit 3" data="2">3</button>
+      <button class="unit-4 unit-button" title="Unit 4" data="3">4</button>
+      <button class="unit-5 unit-button" title="Unit 5" data="4">5</button>
+      <button class="unit-6 unit-button" title="Unit 6" data="5">6</button>
     </div>
     <div class="game-controls">
-        <button class="audio-game" title="audio-game"></button>
-        <button class="sprint-game" title="sprint-game"></button>
+      <button class="audio-game" title="audio-game"></button>
+      <button class="sprint-game" title="sprint-game"></button>
     </div>
-</div>
-<div class="cards-wrap" id="cards-block">  
-</div>
-<div class="pagination-book">
-         <button class="button prev-button" id="prevPageBook" disabled >‹ prev</button>
-         <p class="number-page">${appState.numberPageOfSchoolbook + 1} / ${MAX_NUMBER_OF_WORDS_PER_PAGE}</p>
-         <button class="button next-button" id="nextPageBook">next ›</button>
-</div>
+  </div>
+  <div class="cards-wrap" id="cards-block"></div>
+  <div class="pagination-book">
+    <button class="button prev-button" id="prevPageBook" disabled >‹ prev</button>
+    <p class="number-page">${appState.numberPageOfSchoolbook + 1} / ${MAX_NUMBER_OF_WORDS_PER_PAGE}</p>
+    <button class="button next-button" id="nextPageBook">next ›</button>
+  </div>
 `;
 
 export const cardOfBook = (el: IWord, numericOfUnit: string) => `
-<div class="card-book" id="${el.id}" data-name="${el.word}">
-            <div class="line unit-${numericOfUnit}"></div>
-            <img src="${apiBaseURL}/${el.image}" class="image" width="195" height="130">
-            <div class="english-words">
-                <div class="word">${el.word} ${el.transcription} 
-                <button class="audio-button audio-button-${el.word} " data-audio="${el.audio}" id="audioId"></button>
-                </div>
-                <div class="word-meaning">${el.textMeaning} </div>
-                <div class="word-example">${el.textExample} </div>
-            </div>
-            <div class="russian-words">
-                <div class="word"> ${el.wordTranslate} </div>
-                <div class="word-meaning">${el.textMeaningTranslate}</div>
-                <div class="word-example">${el.textExampleTranslate}</div>
-            </div>
-            <div class="area-learn-difficult hidden" id="area-${el.id}">
-                <button class="learn-button learn-button-${el.word}" title="Изученное" data-name-button = "Studied"></button>
-                <button class="difficult-button difficult-button-${el.word}" title="Сложное" data-name-button = "Heavy"></button>
-                <button class="statistic-button statistic-button-${el.word}" title="Статистика" data-name-button = "Statistic"></button>
-            </div>
-
-        </div>
+  <div class="card-book" id="${el.id}" data-name="${el.word}">
+    <div class="line unit-${numericOfUnit}"></div>
+    <img src="${apiBaseURL}/${el.image}" class="image" width="195" height="130">
+    <div class="english-words">
+      <div class="word">${el.word} ${el.transcription} 
+      <button class="audio-button audio-button-${el.word} " data-audio="${el.audio}" id="audioId"></button>
+      </div>
+      <div class="word-meaning">${el.textMeaning}</div>
+      <div class="word-example">${el.textExample}</div>
+    </div>
+    <div class="russian-words">
+      <div class="word"> ${el.wordTranslate} </div>
+      <div class="word-meaning">${el.textMeaningTranslate}</div>
+      <div class="word-example">${el.textExampleTranslate}</div>
+    </div>
+    <div class="area-learn-difficult hidden" id="area-${el.id}">
+      <button class="learn-button learn-button-${el.word}" title="Изученное" data-name-button = "Studied"></button>
+      <button class="difficult-button difficult-button-${el.word}" title="Сложное" data-name-button = "Heavy"></button>
+      <button class="statistic-button statistic-button-${el.word}" title="Статистика" data-name-button = "Statistic"></button>
+    </div>
+  </div>
 `;

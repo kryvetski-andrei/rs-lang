024d255a14043a9c24b982dd58144e37e76d74a0
alchonokk <galunia_suglob@mail.ru>
2022-02-13 14:05:12 +0300

    feat: add claccName

--- rs-lang/src/components/config.ts
+++ rs-lang/src/components/config.ts
@@ -2,3 +2,7 @@ export const maxCountWordsForPage = 30;
 
 export const pageOfBookLocalStorage = 'pageOfBook';
 export const groupOfSchoolbookLocalStorage = 'groupOfBook';
+
+export const studiedClassName='studied';
+export const difficultClassName='difficult';
+export const hiddenClassName= 'hidden';

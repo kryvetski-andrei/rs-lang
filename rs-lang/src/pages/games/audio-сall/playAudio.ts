3ffb9e4dd7f104ce6206e26859c3d4c84be2bec2
MikolaSh <nikolashulga2000@gmail.com>
2022-02-14 13:27:52 +0300

    refactor: add playAudio function

---
+++ rs-lang/src/pages/games/audio-сall/playAudio.ts
@@ -0,0 +1,8 @@
+import { apiBaseURL } from "../../../utilities/api/config";
+
+export const playAudio = (audioPath: string) => {
+  const audio = new Audio();
+  audio.src = `${apiBaseURL}/${audioPath}`
+  audio.load();
+  audio.play();
+}

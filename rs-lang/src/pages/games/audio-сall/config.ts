61ace013bfb5d096f12756e4e907c4141a9b3261
MikolaSh <nikolashulga2000@gmail.com>
2022-02-14 14:17:41 +0300

    feat: add question count to config

--- rs-lang/src/pages/games/audio-сall/config.ts
+++ rs-lang/src/pages/games/audio-сall/config.ts
@@ -1,3 +1,5 @@
 export const audioCallPageId = 'audio-call';
 export const playAudioIconClassName = 'play-audio';
 export const rightanswerViewerClassName = 'right-answer-viewer';
+export const QUESTIONS_COUNT = 10;
+export const WRONG_VARIANTS_COUNT = 3;

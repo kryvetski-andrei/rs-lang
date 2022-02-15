420c4992101f3fd2d94fbacaad106558846e9b67
MikolaSh <nikolashulga2000@gmail.com>
2022-02-15 21:36:33 +0300

    feat: change loop element name and add class name and data consts

--- rs-lang/src/components/games/audio-сall/components/pushVariants.ts
+++ rs-lang/src/components/games/audio-сall/components/pushVariants.ts
@@ -1,23 +1,23 @@
 import { IAudioCallQuestion } from '../../../../interfaces';
 import { answersContainer } from '../../config';
-import { playAudioIconClassName } from '../config';
+import { audioDataAttribute, playAudioIconClassName, variantItemClassName } from '../config';
 
-const createuQestionVariantDOMElement = (qestionVariant: string) => {
+const getQestionVariantDOMElement = (qestionVariant: string) => {
   const variantDOMElement = document.createElement('button');
-  variantDOMElement.classList.add('variants');
-  variantDOMElement.innerHTML = qestionVariant;
+  variantDOMElement.classList.add(`${variantItemClassName}`);
+  variantDOMElement.innerText = qestionVariant;
   return variantDOMElement;
 };
 
 export const mountQuestionVariantsDOMelements = (quizVariants: Array<IAudioCallQuestion>, currentQuestion: number) => {
   document.body
     .querySelector(`.${playAudioIconClassName}`)
-    ?.setAttribute('data-audio', `${quizVariants[currentQuestion].audio}`);
+    ?.setAttribute(`${audioDataAttribute}`, `${quizVariants[currentQuestion].audio}`);
   const { variants } = quizVariants[currentQuestion];
   (document.body.querySelector(`.${answersContainer}`) as HTMLElement).innerHTML = '';
-  variants.forEach((elem) => {
+  variants.forEach((variant) => {
     document.body
       .querySelector(`.${answersContainer}`)
-      ?.insertAdjacentElement('beforeend', createuQestionVariantDOMElement(elem));
+      ?.insertAdjacentElement('beforeend', getQestionVariantDOMElement(variant));
   });
 };

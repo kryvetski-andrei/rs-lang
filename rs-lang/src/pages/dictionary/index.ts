72fba7e92034e7c958768f4df5e0b4862d788144
alchonokk <galunia_suglob@mail.ru>
2022-02-16 19:17:47 +0300

    feat: add function for mount markup

--- rs-lang/src/pages/dictionary/index.ts
+++ rs-lang/src/pages/dictionary/index.ts
@@ -1,6 +1,14 @@
+import { mountWrapDictionaryDOMElement } from '../../components/dictionary';
+import { createWordsOfDictionary } from '../../components/dictionary/createWordsOfDictionary';
 import { renderMarkup } from '../../utilities/renderMarkup';
+import { dictionaryPageId } from './config';
 import { dictionaryPageMarkup } from './markup';
 
 export const mountDictionaryPageDOMElement = (parentDOMElement: HTMLElement) => {
   renderMarkup(parentDOMElement, dictionaryPageMarkup);
+
+  const dictionaryPageElement = document.body.querySelector(`#${dictionaryPageId}`) as HTMLElement;
+  mountWrapDictionaryDOMElement(dictionaryPageElement);
+
+  createWordsOfDictionary();
 };

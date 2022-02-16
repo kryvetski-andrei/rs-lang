68257ebff30da8ca00e2e1cf38b95607edcb0d7a
MikolaSh <nikolashulga2000@gmail.com>
2022-02-16 23:52:44 +0300

    refactor: rename class name for answer-buttons container

--- rs-lang/src/components/games/config.ts
+++ rs-lang/src/components/games/config.ts
@@ -3,4 +3,4 @@ export const gamesPageId = 'games';
 export const gamePageClassName = 'game-page';
 export const startGameButton = 'start-game';
 export const unitСheckbox = 'unit-item-checkbox';
-export const answersContainer = 'answer-buttons';
+export const answersContainerClassName = 'answer-buttons';

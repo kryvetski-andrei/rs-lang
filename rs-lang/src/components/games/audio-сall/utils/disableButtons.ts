9366bb0dcd861db941c4ee9702735f476e9acdc8
MikolaSh <nikolashulga2000@gmail.com>
2022-02-16 23:56:01 +0300

    feat: add disableButtons function

---
+++ rs-lang/src/components/games/audio-сall/utils/disableButtons.ts
@@ -0,0 +1,7 @@
+import { variantItemClassName } from '../config';
+
+export const disableQuestionVariants = () => {
+  document.body.querySelectorAll(`.${variantItemClassName}`)?.forEach((variant) => {
+    (variant as HTMLButtonElement).disabled = true;
+  })
+};

565c8d30e20d5d680fdaecfaaaafd9171727db2c
MikolaSh <nikolashulga2000@gmail.com>
2022-02-17 15:38:50 +0300

    feat: make getakup functions, make render difficult selection as function

--- rs-lang/src/components/games/sprint/markup.ts
+++ rs-lang/src/components/games/sprint/markup.ts
@@ -11,27 +11,23 @@ import {
   trueAnswerClassName,
   falseAnswerClassName,
 } from './config';
-import { answersContainerClassName, gamePageClassName, pageClassName, unitСheckbox } from '../config';
+import { answersContainerClassName, gamePageClassName, pageClassName } from '../config';
 
 import './index.scss';
+import { getDificultSelectionContainer } from '../utils/getDifficultContainer';
 
-export const sprintPageMarkup = `
+export const sprintPageMarkup = (): string => {
+  return `
   <section id="${sprintPageId}" class="${pageClassName} ${gamePageClassName}">
     <h2 class="game-title">Спринт</h2>
     <p class="game-description">Спринт - это игра, в которой вам будет отображаться пара слово-перевод, а вы должны будете указывать: верный перевод или неверный. Игра на скорость, и на неё вам дается 30 секунд!</p>
     <p class="game-description">Выбирать варианты ответа можно как и нажатием на кнопки "Верно"/"Неверно", так и нажатием клавишь стрелок</p>
     <p class="game-description">Выбирете сложность</p>
-    <div class="difficulty-selection-container">
-      <input class="${unitСheckbox}" type="radio" id="1" name="unit"><label class="unit-item" for="1">A1</label>
-      <input class="${unitСheckbox}" type="radio" id="2" name="unit"><label class="unit-item" for="2">A2</label>
-      <input class="${unitСheckbox}" type="radio" id="3" name="unit"><label class="unit-item" for="3">B1</label>
-      <input class="${unitСheckbox}" type="radio" id="4" name="unit"><label class="unit-item" for="4">B2</label>
-      <input class="${unitСheckbox}" type="radio" id="5" name="unit"><label class="unit-item" for="5">C1</label>
-      <input class="${unitСheckbox}" type="radio" id="6" name="unit"><label class="unit-item" for="6">C2</label>
-    </div>
+    ${localStorage.getItem('previousPage') === 'games' ? getDificultSelectionContainer : ''}
     <button class="start-game">Начать</button>
   </section>
 `;
+};
 
 export const resultsMarkup = `
 <div class="results-viewer">

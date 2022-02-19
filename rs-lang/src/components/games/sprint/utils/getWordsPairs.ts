e0ac8a53513d0e95bb41a30397c162d8919aaa3b
MikolaSh <nikolashulga2000@gmail.com>
2022-02-20 01:40:32 +0300

    feat: add id to airs

--- rs-lang/src/components/games/sprint/utils/getWordsPairs.ts
+++ rs-lang/src/components/games/sprint/utils/getWordsPairs.ts
@@ -4,11 +4,11 @@ import { shuffle } from '../../utils/shuffle';
 
 export const generatePairs = (wordsData: Array<IWord>): Array<IPair> => {
   const wordsPpairs: Array<IPair> = [];
-  wordsData.forEach(({ word, wordTranslate }, index) => {
+  wordsData.forEach(({ id, word, wordTranslate }, index) => {
     if (Math.random() < 0.5) {
-      wordsPpairs.push({ wordsPair: `${word} — ${wordTranslate}`, isPairRight: true });
+      wordsPpairs.push({ id, wordsPair: `${word} — ${wordTranslate}`, isPairRight: true });
     } else {
-      wordsPpairs.push({ wordsPair: `${word} — ${getRandomTranslation(wordsData, index)}`, isPairRight: false });
+      wordsPpairs.push({ id, wordsPair: `${word} — ${getRandomTranslation(wordsData, index)}`, isPairRight: false });
     }
   });
   shuffle(wordsPpairs);

a9c23249b8a6e770e5c37a891747711ca6568f08
alchonokk <galunia_suglob@mail.ru>
2022-02-19 22:49:24 +0300

    feat: add classes in config

--- rs-lang/src/components/config.ts
+++ rs-lang/src/components/config.ts
@@ -12,9 +12,12 @@ export const markOfDifficultWord = 'hard';
 export const markOfNot = 'no';
 export const infoClassName ='info-dictionary';
 
-
 export const sprintGameClassName = '.sprint-game';
 export const audioGameClassName = '.audio-game';
 export const gameSprintElement = document.body.querySelector(sprintGameClassName) as HTMLButtonElement;
 export const gameAudioElement = document.body.querySelector(audioGameClassName) as HTMLButtonElement;
 export const boxGamesClassName = '.games-dictionary';
+
+export const markedWordsClassName = 'all';
+export const inactiveLinkClassName = 'inactive-link';
+export const wrapOfWordsClassName = '.cards-wrap';

132e86d0ee4228e54e5deff43ae7d527e4b01064
alchonokk <galunia_suglob@mail.ru>
2022-02-19 22:57:03 +0300

    feat: add checkStatusPage

--- rs-lang/src/components/schoolbook/createCardsOfWord.ts
+++ rs-lang/src/components/schoolbook/createCardsOfWord.ts
@@ -4,6 +4,7 @@ import { appState, boxCardsIdName } from '../../pages/schoolbook/config';
 import { getUserWords, getWords } from '../../utilities/api';
 import { userDataLocalStorage } from '../../utilities/api/config';
 import { cleanUp } from '../../utilities/cleanUp';
+import { checkStatusPage } from './checkStatusOfWord';
 import { addPageOfBookInLocalStorage, addUnitOfBookInLocalStorage } from './localStorageOfBook';
 import { showArea } from './showAreaDifficultAndLearn';
 import { switchAudio } from './switchAudio';
@@ -24,15 +25,17 @@ export const createCardsOfBook = async () => {
   cleanUp(cardOfBookElement);
 
   const numericOfUnit = String(appState.groupOfSchoolbook + 1);
-
+  
   if (localStorage.getItem(`${userDataLocalStorage}`)) {
     const userDataForBook = JSON.parse(localStorage.getItem(`${userDataLocalStorage}`)!);
     const arrayOfUserWord = await getWordsOfUser(userDataForBook.userId);
+
     wordsForPage.forEach((wordOfPage: IWord) => {
       mountCardOfBookDOMElement(cardOfBookElement, wordOfPage, numericOfUnit);
       switchAudio(wordOfPage.word);
       showArea(wordOfPage.word, wordOfPage.id, userDataForBook.userId, arrayOfUserWord);
     });
+    checkStatusPage();
   } else {
     wordsForPage.forEach((wordOfPage: IWord) => {
       mountCardOfBookDOMElement(cardOfBookElement, wordOfPage, numericOfUnit);
@@ -40,3 +43,4 @@ export const createCardsOfBook = async () => {
     });
   }
 };
+

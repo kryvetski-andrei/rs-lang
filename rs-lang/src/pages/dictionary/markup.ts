95b8571b61b636e2f47ed97ba6c8ad173a73d347
alchonokk <galunia_suglob@mail.ru>
2022-02-19 20:54:24 +0300

    feat: add title for dictionary

--- rs-lang/src/pages/dictionary/markup.ts
+++ rs-lang/src/pages/dictionary/markup.ts
@@ -4,5 +4,5 @@ import { dictionaryPageId } from './config';
 import './index.scss';
 
 export const dictionaryPageMarkup = `
-  <section id="${dictionaryPageId}" class="${pageClassName}">Dictionary</section>
+  <section id="${dictionaryPageId}" class="${pageClassName}"><h2 class="title-dictionary">Словарь</h2></section>
 `;

d88dfbe5a3096e2bdab6cbd160dba015ea03930d
kryvetski-andrei <kryvetskistud@gmail.com>
2022-02-20 03:47:18 +0300

    feat: add 3d books with three.js

--- rs-lang/src/pages/home/index.ts
+++ rs-lang/src/pages/home/index.ts
@@ -1,6 +1,17 @@
+import { mountBooksBGDOMElement } from '../../components/booksBg';
 import { renderMarkup } from '../../utilities/renderMarkup';
+import { homePageId } from './config';
 import { homePageMarkup } from './markup';
+
+
+
+
 
 export const mountHomePageDOMElement = (parentDOMElement: HTMLElement) => {
   renderMarkup(parentDOMElement, homePageMarkup);
+
+  const homePageDOMElement = document.body.querySelector(`#${homePageId}`) as HTMLElement;
+  mountBooksBGDOMElement(homePageDOMElement);
 };
+
+

cdb8ee20d9496b20ee97629a041827b7beb5da48
MikolaSh <nikolashulga2000@gmail.com>
2022-02-22 00:20:28 +0300

    feat: move setLearnStatusto module

--- rs-lang/src/components/games/sprint/utils/gamePlay.ts
+++ rs-lang/src/components/games/sprint/utils/gamePlay.ts
@@ -2,9 +2,9 @@ import { IPair } from '../../../../interfaces';
 import { deleteUserWord, getUserWord, postUsersWords } from '../../../../utilities/api';
 import { userDataLocalStorage } from '../../../../utilities/api/config';
 import { markOfDifficultWord, markOfLearnedWord, markOfNot } from '../../../config';
-import { isHardWord } from '../../utils/isHardWord';
 import { isUserWord } from '../../utils/isUserWord';
 import { playAnswerSound } from '../../utils/playAnswerSound';
+import { setLearnStatus } from '../../utils/setLearnStatus';
 import { wordTranslation } from '../config';
 
 export const showCurrentPair = ({ wordsPair }: IPair) => {
@@ -16,29 +16,7 @@ export const setAnswer = async (wordPair: IPair, userAnswer: string) => {
   console.log(wordPair);
   const { userId } = JSON.parse(localStorage.getItem(`${userDataLocalStorage}`) as string);
   wordPair.userCorrect = String(wordPair.isPairRight) === userAnswer;
-  if (wordPair.userCorrect) {
-    const learnOption = { difficulty: markOfNot, optional: { learn: markOfLearnedWord, word } };
-    if (await isUserWord(id, userId)) {
-      const { optional } = await getUserWord(userId, id);
-      if (optional.learn === 'no') {
-        await deleteUserWord(userId, id);
-        await postUsersWords(userId, id, learnOption);
-      }
-    } else {
-      await postUsersWords(userId, id, learnOption);
-    }
-  } else {
-    const difficultOption = { difficulty: markOfDifficultWord, optional: { learn: markOfNot, word } };
-    if (await isUserWord(id, userId)) {
-      const { optional } = await getUserWord(userId, id);
-      if (optional.learn === 'learn') {
-        await deleteUserWord(userId, id);
-        await postUsersWords(userId, id, difficultOption);
-      }
-    } else {
-      await postUsersWords(userId, id, difficultOption);
-    }
-  }
+  await setLearnStatus(wordPair, userId, id, word);
 
   playAnswerSound(wordPair.userCorrect as boolean);
 };

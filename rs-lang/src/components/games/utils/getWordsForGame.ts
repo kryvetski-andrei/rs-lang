90e7856a3ccfd9c1b628714a0176d17d5d4c3fdf
MikolaSh <nikolashulga2000@gmail.com>
2022-02-21 14:17:47 +0300

    feat: return words from dictionary

--- rs-lang/src/components/games/utils/getWordsForGame.ts
+++ rs-lang/src/components/games/utils/getWordsForGame.ts
@@ -1,27 +1,39 @@
-import { getWords } from '../../../utilities/api';
-import { gamesPageName, groupOfBook, pageOfBook, previousPage, schoolbookPageName } from '../config';
+import { IWord, IWordDictionaryElement } from '../../../interfaces';
+import { getUserAggregatedHardWords, getWords } from '../../../utilities/api';
+import { userDataLocalStorage } from '../../../utilities/api/config';
+import {
+  dictionaryPageName,
+  gamesPageName,
+  groupOfBook,
+  pageOfBook,
+  previousPage,
+  schoolbookPageName,
+} from '../config';
 import { getCurrentGroupOfWords } from './getCurrentGroup';
+import { parseDictionaryElement } from './parseDictionaryElement';
 
 export const getWordsForGame = async (): Promise<any> => {
+  console.log(localStorage.getItem(previousPage));
   if (localStorage.getItem(previousPage) === gamesPageName) {
-    return getWords(0, getCurrentGroupOfWords());
+    return getWords(1, getCurrentGroupOfWords());
   }
   if (localStorage.getItem(previousPage) === schoolbookPageName) {
-    return getWords(Number(localStorage.getItem(pageOfBook)), Number(localStorage.getItem(groupOfBook)));
+    const test = await getWords(Number(localStorage.getItem(pageOfBook)), Number(localStorage.getItem(groupOfBook)));
+    const wordIDs = test.map(({ id }: any) => {
+      return id;
+    });
+    console.log(test);
+    return test;
   }
-};
-
-const initialStatistics = {
-  learnedWords: 0,
-  optional: {
-    newWords: [],
-    audioCallGameStat: {
-      longestSeries: 0,
-      words: [],
-    },
-    springGameStat: {
-      longestSeries: 0,
-      words: [],
-    },
-  },
+  if (localStorage.getItem(previousPage) === dictionaryPageName) {
+    console.log(localStorage.getItem(previousPage));
+    const { userId } = JSON.parse(localStorage.getItem(`${userDataLocalStorage}`) as string);
+    console.log(userId);
+    const tet = await getUserAggregatedHardWords(userId);
+    const out: Array<IWord> = tet[0].paginatedResults.map((dictionaryElement: IWordDictionaryElement) => {
+      return parseDictionaryElement(dictionaryElement);
+    })
+    console.log(out);
+    return out;
+  }
 };

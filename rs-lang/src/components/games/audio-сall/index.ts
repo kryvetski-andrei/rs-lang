5a434ae1be16cd36c80d3e2d58d325a13ada90af
MikolaSh <nikolashulga2000@gmail.com>
2022-02-22 00:21:07 +0300

    feat: keyboard controls

--- rs-lang/src/components/games/audio-сall/index.ts
+++ rs-lang/src/components/games/audio-сall/index.ts
@@ -9,20 +9,30 @@ import { setResults } from '../utils/setResults';
 import { mountQuestionVariantsDOMelements } from './components/pushVariants';
 import { renderAudioCallGame } from './components/renderAudioCallGame';
 import { showRightAnswer } from './components/showRightAnswer';
-import { audioCallPageId, audioDataAttribute, playAudioIconClassName, rightAnswerViewerClassName } from './config';
+import {
+  audioCallPageId,
+  audioDataAttribute,
+  nextQuestionButtonClassName,
+  playAudioIconClassName,
+  rightAnswerViewerClassName,
+  variantItemClassName,
+} from './config';
 import { audioCallPageMarkup } from './markup';
 import { disableQuestionVariants } from './utils/disableButtons';
 import { generateQuizQuestions } from './utils/generateQuestions';
 import { playAudio } from './utils/playAudio';
 import { getUserStatistics } from '../../../utilities/api';
 import { TokenService } from '../../../utilities/api/utilities';
+import { setLearnStatus } from '../utils/setLearnStatus';
 
 const setAnswer = async (currentQuestion: IAudioCallQuestion, target: HTMLElement) => {
   const { userId } = TokenService.getUser();
   const userStatistics = await getUserStatistics(userId);
+  const { id, rightAnswer } = currentQuestion;
 
   currentQuestion.userCorrect = currentQuestion.rightAnswer === target.innerHTML;
   playAnswerSound(currentQuestion.userCorrect);
+  setLearnStatus(currentQuestion, userId, id, rightAnswer);
   setNewWord(userStatistics, currentQuestion);
   setAudioCallGameStat(userStatistics, currentQuestion);
 };
@@ -41,6 +51,7 @@ const startGameAudioGame = async () => {
   };
   let currentQuestion = 0;
   const quizVariants = generateQuizQuestions(await getWordsForGame());
+  console.log(quizVariants);
   const audioCallContainer = document.body.querySelector(`#${audioCallPageId}`) as HTMLElement;
   renderAudioCallGame(audioCallContainer, quizVariants, currentQuestion);
 
@@ -50,6 +61,11 @@ const startGameAudioGame = async () => {
 
   audioCallContainer.querySelector(`.${playAudioIconClassName}`)?.addEventListener('click', () => {
     playAudio(audioButton?.getAttribute(`${audioDataAttribute}`) as string);
+  });
+
+  audioCallContainer.querySelector(`.${nextQuestionButtonClassName}`)?.addEventListener('click', () => {
+    changeQuestion(quizVariants, currentQuestion);
+    document.body.querySelector(`.${nextQuestionButtonClassName}`)?.classList.toggle('hide');
   });
 
   audioCallContainer.querySelector(`.${answersContainerClassName}`)?.addEventListener('click', async ({ target }) => {
@@ -68,10 +84,31 @@ const startGameAudioGame = async () => {
         setResults(audioCallRsults, quizVariants[currentQuestion]);
         showRightAnswer(quizVariants[currentQuestion], targetElement);
         disableQuestionVariants();
+        document.body.querySelector(`.${nextQuestionButtonClassName}`)?.classList.toggle('hide');
         currentQuestion += 1;
-        setTimeout(() => {
-          changeQuestion(quizVariants, currentQuestion);
-        }, 3000);
+      }
+    }
+  });
+
+  window.addEventListener('keyup', async (e) => {
+    const { userId } = TokenService.getUser();
+    const userStatistics = await getUserStatistics(userId);
+    const answerElements = document.body.querySelectorAll(`.${variantItemClassName}`);
+    const userAnswerNumber = (e.code.match(/[1-4]$/) as Array<any>)[0];
+    if (userAnswerNumber) {
+      if (currentQuestion === quizVariants.length - 1) {
+        await setAnswer(quizVariants[currentQuestion], answerElements[userAnswerNumber - 1] as HTMLElement);
+        setResults(audioCallRsults, quizVariants[currentQuestion]);
+        currentQuestion = 0;
+        showResults(quizVariants, audioCallContainer);
+        setAudioCallBestSeries(userStatistics, audioCallRsults);
+      } else {
+        await setAnswer(quizVariants[currentQuestion], answerElements[userAnswerNumber - 1] as HTMLElement);
+        setResults(audioCallRsults, quizVariants[currentQuestion]);
+        showRightAnswer(quizVariants[currentQuestion], answerElements[userAnswerNumber - 1] as HTMLElement);
+        document.body.querySelector(`.${nextQuestionButtonClassName}`)?.classList.toggle('hide');
+        disableQuestionVariants();
+        currentQuestion += 1;
       }
     }
   });

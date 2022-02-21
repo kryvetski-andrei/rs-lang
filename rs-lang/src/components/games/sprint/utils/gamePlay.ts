8980f514d7460d17156faa567eb3f33b6628807f
MikolaSh <nikolashulga2000@gmail.com>
2022-02-21 20:28:44 +0300

    feat: move words to learn or hard section

--- rs-lang/src/components/games/sprint/utils/gamePlay.ts
+++ rs-lang/src/components/games/sprint/utils/gamePlay.ts
@@ -1,4 +1,9 @@
 import { IPair } from '../../../../interfaces';
+import { deleteUserWord, getUserWord, postUsersWords } from '../../../../utilities/api';
+import { userDataLocalStorage } from '../../../../utilities/api/config';
+import { markOfDifficultWord, markOfLearnedWord, markOfNot } from '../../../config';
+import { isHardWord } from '../../utils/isHardWord';
+import { isUserWord } from '../../utils/isUserWord';
 import { playAnswerSound } from '../../utils/playAnswerSound';
 import { wordTranslation } from '../config';
 
@@ -6,7 +11,34 @@ export const showCurrentPair = ({ wordsPair }: IPair) => {
   (document.body.querySelector(`.${wordTranslation}`) as HTMLElement).innerHTML = `${wordsPair}`;
 };
 
-export const setAnswer = (wordPair: IPair, userAnswer: string) => {
+export const setAnswer = async (wordPair: IPair, userAnswer: string) => {
+  const { id, word } = wordPair;
+  console.log(wordPair);
+  const { userId } = JSON.parse(localStorage.getItem(`${userDataLocalStorage}`) as string);
   wordPair.userCorrect = String(wordPair.isPairRight) === userAnswer;
+  if (wordPair.userCorrect) {
+    const learnOption = { difficulty: markOfNot, optional: { learn: markOfLearnedWord, word } };
+    if (await isUserWord(id, userId)) {
+      const { optional } = await getUserWord(userId, id);
+      if (optional.learn === 'no') {
+        await deleteUserWord(userId, id);
+        await postUsersWords(userId, id, learnOption);
+      }
+    } else {
+      await postUsersWords(userId, id, learnOption);
+    }
+  } else {
+    const difficultOption = { difficulty: markOfDifficultWord, optional: { learn: markOfNot, word } };
+    if (await isUserWord(id, userId)) {
+      const { optional } = await getUserWord(userId, id);
+      if (optional.learn === 'learn') {
+        await deleteUserWord(userId, id);
+        await postUsersWords(userId, id, difficultOption);
+      }
+    } else {
+      await postUsersWords(userId, id, difficultOption);
+    }
+  }
+
   playAnswerSound(wordPair.userCorrect as boolean);
 };

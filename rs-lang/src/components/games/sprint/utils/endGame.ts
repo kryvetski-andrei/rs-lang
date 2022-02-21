ea3a8ed38873d797c2af38b286817eaa7f061131
MikolaSh <nikolashulga2000@gmail.com>
2022-02-21 15:28:12 +0300

    feat: now show redults with count and audio

--- rs-lang/src/components/games/sprint/utils/endGame.ts
+++ rs-lang/src/components/games/sprint/utils/endGame.ts
@@ -1,25 +1,45 @@
-import { IPair, IAudioCallQuestion } from '../../../../interfaces';
+import { IPair, IAudioCallQuestion, IResults } from '../../../../interfaces';
 import { renderMarkup } from '../../../../utilities/renderMarkup';
-import { rightAnswersContainerClassName, wrongAnswersContainerClassName } from '../config';
+import { audioDataAttribute, playAudioIconClassName } from '../../audio-сall/config';
+import { playAudio } from '../../audio-сall/utils/playAudio';
+import { getAudioIconSVG } from '../../markup';
+import {
+  playAgainButtonClassName,
+  resultsContainerClassName,
+  RESULT_ICON_SIZE,
+  rightAnswersContainerClassName,
+  wrongAnswersContainerClassName,
+} from '../config';
 import { resultsMarkup } from '../markup';
 
 const pushVariantsToList = (wordPairs: Array<IPair | IAudioCallQuestion>, parentDOMElement: HTMLElement) => {
+  let rightAnswerCount = 0;
+  let wrongAnswerCount = 0;
   const rightAnswersContainer = parentDOMElement.querySelector(`.${rightAnswersContainerClassName}`);
   const wrongAnswersContainer = parentDOMElement.querySelector(`.${wrongAnswersContainerClassName}`);
-  wordPairs.forEach(({ wordsPair, userCorrect }) => {
+  wordPairs.forEach(({ audio, wordsPair, userCorrect }) => {
     if (userCorrect !== undefined) {
       const listItem = document.createElement('li');
-      listItem.innerHTML = `${wordsPair}`;
+      listItem.innerHTML = `${getAudioIconSVG(RESULT_ICON_SIZE, audio)}${wordsPair}`;
       if (userCorrect) {
+        rightAnswerCount += 1;
         rightAnswersContainer?.insertAdjacentElement('beforeend', listItem);
       } else {
+        wrongAnswerCount += 1;
         wrongAnswersContainer?.insertAdjacentElement('beforeend', listItem);
       }
     }
   });
+  (document.querySelector('.right') as HTMLElement).innerHTML = `Верно: ${rightAnswerCount}`;
+  (document.querySelector('.wrong') as HTMLElement).innerHTML = `Неверно: ${wrongAnswerCount}`;
 };
 
 export const showResults = (wordPairs: Array<IPair | IAudioCallQuestion>, parentDOMElement: HTMLElement) => {
   renderMarkup(parentDOMElement, resultsMarkup);
   pushVariantsToList(wordPairs, parentDOMElement);
+  parentDOMElement.querySelectorAll(`.${playAudioIconClassName}`)?.forEach((playButtonImage) => {
+    playButtonImage.addEventListener('click', () => {
+      playAudio(playButtonImage.getAttribute(`${audioDataAttribute}`) as string);
+    });
+  });
 };
